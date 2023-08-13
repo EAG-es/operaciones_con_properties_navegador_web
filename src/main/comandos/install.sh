f36af74dd548c542b72ee3cfb749389cf0692365
emilio <eag2001@gmail.com>
2023-08-13 14:01:20 +0100

    20230813

---
+++ src/main/comandos/install.sh
@@ -0,0 +1,8 @@
+echo Requisitos:
+echo Java 17 o superior. \(https://www.oracle.com/es/java/technologies/downloads/#java17\)
+echo
+echo Iniciando la instalación de JavaFX desde la aplicación: configurar_javafx
+java -jar configurar_javafx-1.17.0.8.jar
+echo
+echo Instalación terminada. Para lanzar la aplicación, puede utilizar: run.bat
+echo 
---
+++ src/main/comandos/run.bat
@@ -0,0 +1,5 @@
+echo Requisitos:
+echo Java 17 o superior. \(https://www.oracle.com/es/java/technologies/downloads/#java17\)
+echo
+java --module-path .:re/linux: --add-modules javafx.controls,javafx.fxml,javafx.web,javafx.media,javafx.graphics,javafx.base,javafx.swing -jar operaciones_con_properties_navegador_web-1.0.jar
+echo 
